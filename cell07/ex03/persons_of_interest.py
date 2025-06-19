2e8aab7fb051efd8ddae6456adda47ba164a1842
AOM101049 <AOMBURAPORN@GMAIL.COM>
2025-06-19 03:28:50 +0000

    7.3

--- cell07/ex03/persons_of_interest.py
+++ cell07/ex03/persons_of_interest.py
@@ -1,3 +1,19 @@
-def famous_births(peopie):
+def famous_births(people):
     # แปลง dict เป็น list แล้งเรียงลำดับตามปีเกิด
-    sorted_people = sorted(people.
+    sorted_people = sorted(people.values(), key=lambda p: p['date_ofbirth'])
+
+    for person in sorted_people:
+        print(f"{person['name']} is a great scientist born in {person['date_of_birth']}.") 
+
+
+women_scientists = {
+            "ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
+            "cecilia": {"name": "Cecilia Payne", "date_of_birth": "1900" },
+            "lise": {"name": "lise Payne", "date_of_birth": "1878" },
+            "grace": { "name": "Grace Hopper", "date_of_birth": "1906"}
+}
+
+famous_births(women_scientists)
+
+
+
